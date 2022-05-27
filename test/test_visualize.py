520eeb34e4eebc24d91762bb55b18fbab3f29aee
SeanMcCarren <mccarrensean@gmail.com>
2022-05-27 10:37:58 +0200

    Update test_visualize.py

--- test/test_visualize.py
+++ test/test_visualize.py
@@ -2,6 +2,6 @@ from visualize_events.snomed.visualize import *
 from visualize_events.snomed.dag import *
 from test_dag import predictions_dag, diamond_dag, diamond_dag_tail
 
-T = predictions_dag().compact_preds()
-
-draw_dag(T)
+def test_draw_dag():
+    T = predictions_dag().compact_preds()
+    draw_dag(T)
